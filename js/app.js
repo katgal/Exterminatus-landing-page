a1772fbab3a81e76c988f2084cdba925f773a8f7
Katarzyna Galas <katarzyna.galas2@gmail.com>
2016-10-05 18:34:50 +0200

    adding signature

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
+/*created by katgal october 2016*/
 $(document).ready(function() {
 
-  // console.log("Yes Master? ...We will build it for you.");
-  console.log("Do you hear the voices too?");
+    // console.log("Yes Master? ...We will build it for you.");
+    console.log("Do you hear the voices too?");
 });
