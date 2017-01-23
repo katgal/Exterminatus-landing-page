0ebf4a4c33e84c032f96d1708c37608cda5dcac6
Katarzyna Galas <katarzyna.galas2@gmail.com>
2017-01-23 14:20:25 +0100

    Sanity is for the waek - renamed file which did not displayed on win 8.1; removed experimental hotfix

--- js/app.js
+++ js/app.js
@@ -415,16 +415,3 @@ function mottoSlider() {
 
 }
 mottoSlider();
-
-//experimantal fix for fixed background stuttering on IE Windows 8.1
-if(navigator.userAgent.match(/Trident\/7\./)) { // if IE
-        $('body').on("mousewheel", function () {
-            // remove default behavior
-            event.preventDefault();
-
-            //scroll without smoothing
-            var wheelDelta = event.wheelDelta;
-            var currentScrollPosition = window.pageYOffset;
-            window.scrollTo(0, currentScrollPosition - wheelDelta);
-        });
-}
